4e03edba1a6024ede07cc70ddae5483cc31f2507
JB <johnpbills@gmail.com>
2024-03-28 19:27:47 +0800

    removed

--- _togglcon.bat
+++
@@ -1,6 +0,0 @@
-REM Replace ###user### with the correct path. Rename this file to remove _.
-
-@echo OFF
-call C:\Users\###user###\Anaconda3\Scripts\activate.bat
-cd C:\Users\###user###\Code\TogglCon
-python togglcon.py
