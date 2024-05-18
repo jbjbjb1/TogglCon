abaff61d56e783a9104cf2801ad4c932d64ddf20
JB <johnpbills@gmail.com>
2024-05-18 19:44:49 +0800

    Updated toggl api

--- togglcon.py
+++ togglcon.py
@@ -5,7 +5,7 @@ from time import sleep
 
 
 # Version and welcome message
-version = '3.10'
+version = '3.10.1'
 print(f'---> togglcon, version {version} <---')
 
 def get_and_handle_timesheet(date):
